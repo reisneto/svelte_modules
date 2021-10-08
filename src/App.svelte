418be381ac0317de12bf4e1ff4903efdc4b88a35
Álvaro Reis <reisneto.alvaro@gmail.com>
2021-10-08 19:59:08 -0400

    feat: create card component

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  import Card from './Card.svelte';
+</script>
+
+<main>
+  <div class="container">
+    <Card
+      headline="Olá Mundo!"
+      subheadline="Esse é meu primeiro teste utilizando Svelte"
+      cta="Quero aprender Svelte também"
+    />
+  </div>
+</main>
+
+<style>
+	:global(body){
+		background-color: darkslateblue;
+	}
+
+  main {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    height: 80vh;
+  }
+
+  .container {
+    position: relative;
+    width: 60%;
+    height: auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/Card.svelte
@@ -0,0 +1,46 @@
+<script>
+  export let headline;
+	export let subheadline;
+	export let cta;
+</script>
+
+<div class="card">
+  <h2 class="card__headline">{headline}</h2>
+  <p class="card__subheadline">{subheadline}</p>
+  <button class="card__button"><a href="http://alvaroreisneto.com">{cta}</a></button>
+</div>
+
+<style>
+  .card {
+    background-color: whitesmoke;
+    position: relative;
+    width: 100%;
+    text-align: center;
+    padding: 20px;
+    box-shadow: 3px 8px 31px -4px rgba(0,0,0,0.34);
+  }
+
+  .card__headline {
+    font-size: 24px;
+    font-weight: 600;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    color: darkslategray;
+  }
+
+  .card__subheadline {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 18px;
+    color: slategray;
+  }
+
+  .card__button {
+    padding: 10px 20px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 18px;
+    background-color: goldenrod;
+  }
+
+  .card__button > a { 
+    color: white;
+  }
+</style>
